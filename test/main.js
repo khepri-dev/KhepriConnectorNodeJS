24d5b9e87cea809b3d7ee043c9e9761671138c12
Gregory Gerard <greg@gregs>
2016-06-22 15:00:41 +0200

    Structure et fichiers

---
+++ test/main.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+"use strict";
+var khepriObj = require('./khepriApi');
+
+
+
+var urlKhepri = 'http://test-khepridevelba-1t8aalo9fz0t1-723475646.eu-west-1.elb.amazonaws.com';
+var apiKey = '213e71dd6e9f354de2c42eee366d4263';
+var instanceId = 3;
+
+var khepriTest = new khepriApi();
+//khepriApi.init(urlKhepri, apiKey);
+//khepriTest.init(urlKhepri, apiKey);
+
+//khepriApi.ask(instanceId);
+
