40cbad2105f098283c5b84a7444409889db051cb
GregoryGerard <gregory.gerard@wister.fr>
2016-06-22 16:47:04 +0200

    Fucntionnal Call

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 "use strict";
-var khepriObj = require('./khepriApi');
+var khepriObj = require('../lib/KhepriApi');
 
 
 
@@ -8,9 +8,10 @@ var urlKhepri = 'http://test-khepridevelba-1t8aalo9fz0t1-723475646.eu-west-1.elb
 var apiKey = '213e71dd6e9f354de2c42eee366d4263';
 var instanceId = 3;
 
-var khepriTest = new khepriApi();
-//khepriApi.init(urlKhepri, apiKey);
+
+//var khepriTest = new khepriObj();
+khepriObj.init(urlKhepri, apiKey);
 //khepriTest.init(urlKhepri, apiKey);
 
-//khepriApi.ask(instanceId);
+khepriObj.ask(instanceId);
 
